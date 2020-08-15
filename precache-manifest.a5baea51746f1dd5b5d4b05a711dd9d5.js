77e760817c54b46991ccacf30af5c2b2f10ca254
prasannabalaji7 <prasannabalaji07@gmail.com>
2020-08-15 18:25:20 +0800

    Updates

---
+++ precache-manifest.a5baea51746f1dd5b5d4b05a711dd9d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2fe0264c576e24f973a103b4873c254",
+    "url": "/user-form/index.html"
+  },
+  {
+    "revision": "d33ac4faede18f1d0bef",
+    "url": "/user-form/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3f3eea3c885c2e65bc0e",
+    "url": "/user-form/static/css/main.bdbef3a1.chunk.css"
+  },
+  {
+    "revision": "d33ac4faede18f1d0bef",
+    "url": "/user-form/static/js/2.f065e38c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/user-form/static/js/2.f065e38c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f3eea3c885c2e65bc0e",
+    "url": "/user-form/static/js/main.fce919bd.chunk.js"
+  },
+  {
+    "revision": "408f4cf17d5fbc1d4881",
+    "url": "/user-form/static/js/runtime-main.847e35dd.js"
+  }
+]);
